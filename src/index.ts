199054d0f59a66a92c840512ec779996f95b6179
Vlad Mustiata <vlad@deqod.com>
2022-02-08 17:57:27 +0200

    refactor: added more supported origins

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ class DigitapGamePlayerSDK {
     "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://localhost:3001",
     "http://127.0.0.1:3001",
     "https://127.0.0.1:3001",
   ];
